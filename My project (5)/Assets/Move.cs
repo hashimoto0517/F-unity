64f4be37e2c879fd243c3ff5732b7bb6f3c16d8e
hashimoto0517 <izumi.gsmz1@gmail.com>
2025-10-14 17:30:42 +0900

    heya

--- My project (5)/Assets/Move.cs
+++ My project (5)/Assets/Move.cs
@@ -3,12 +3,21 @@ using UnityEngine.InputSystem;
 
 public class Move : MonoBehaviour
 {
-    public float speed = 5f; // ˆÚ“®‘¬“x
+    public float speed = 20f; // ˆÚ“®‘¬“x
+
+    float posPlusLim;
+    float posMinusLim;
+
     //public float rotationSpeed = 700f; // ‰ñ“]‘¬“x
-   
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
+        Vector3 pos = transform.position;
+        posPlusLim = pos.x + 39f;
+        posMinusLim = pos.x - 39f;
+
         int maxDisplayCount = 2;
         for (int i = 0; i < maxDisplayCount && i < Display.displays.Length; i++)
         {
@@ -24,8 +33,8 @@ public class Move : MonoBehaviour
         transform.Translate(move * Time.deltaTime * speed);
 
         Vector3 pos = transform.position;
-        pos.x = Mathf.Clamp(pos.x, -14.5f, 14.5f);
-        pos.z = Mathf.Clamp(pos.z, -14.5f, 14.5f);
+        pos.x = Mathf.Clamp(pos.x, posMinusLim, posPlusLim);
+        pos.z = Mathf.Clamp(pos.z, posMinusLim, posPlusLim);
         transform.position = pos;
     }
 }
--- My project (5)/Assets/hEdit/MoveTesScript.cs
+++ My project (5)/Assets/hEdit/MoveTesScript.cs
@@ -3,12 +3,19 @@ using UnityEngine.InputSystem;
 
 public class MoveTes : MonoBehaviour
 {
-    public float speed = 5f; // ˆÚ“®‘¬“x
+    public float speed = 20f; // ˆÚ“®‘¬“x
                              //public float rotationSpeed = 700f; // ‰ñ“]‘¬“x
+
+    float posPlusLim;
+    float posMinusLim;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Vector3 pos = transform.position;
+        posPlusLim = pos.x + 39f;
+        posMinusLim = pos.x - 39f;
+
         int maxDisplayCount = 2;
         for (int i = 0; i < maxDisplayCount && i < Display.displays.Length; i++)
         {
@@ -24,8 +31,8 @@ public class MoveTes : MonoBehaviour
         transform.Translate(move * Time.deltaTime * speed);
 
         Vector3 pos = transform.position;
-        pos.x = Mathf.Clamp(pos.x, -14.5f, 14.5f);
-        pos.z = Mathf.Clamp(pos.z, -14.5f, 14.5f);
+        pos.x = Mathf.Clamp(pos.x, posMinusLim, posPlusLim);
+        pos.z = Mathf.Clamp(pos.z, posMinusLim, posPlusLim);
         transform.position = pos;
     }
 }
