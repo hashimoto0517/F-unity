7a6b281fb4e585fb96bfb105c683c9064e476d28
hashimoto0517 <izumi.gsmz1@gmail.com>
2026-01-04 02:13:28 +0900

    tyotto

---
+++ My project (5)/Assets/hEdit/ResultScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ResultDataScript : MonoBehaviour
+{
+    public static int life;        // 残りライフ
+    public static float time;      // 残りタイム
+    public static int correct;     // 見つけた間違い数
+    public static int totalCorrect; // 全部の間違い数
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
